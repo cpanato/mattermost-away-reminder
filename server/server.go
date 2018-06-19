d455c616999e2d814fa3e4fabab1788f94b4ea9e
cpanato <ctadeu@gmail.com>
2018-06-19 20:33:26 +0200

    update

--- server/server.go
+++ server/server.go
@@ -61,12 +61,18 @@ func Start() {
 
 func addApis(r *mux.Router) {
 	r.HandleFunc("/", indexHandler).Methods("GET", "POST")
+	r.HandleFunc("/ping", pingHandler).Methods("GET")
 	// r.HandleFunc("/delete", deleteUserAways).Methods("POST")
-	r.HandleFunc("/test", slashCommandHandler).Methods("POST")
+	r.HandleFunc("/time_away", slashCommandHandler).Methods("POST")
 }
 
 func indexHandler(res http.ResponseWriter, req *http.Request) {
-	res.Write([]byte("This is the mattermost Time away bot server."))
+	res.Write([]byte("This is the mattermost Time Away Bot Server."))
+}
+
+func pingHandler(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("ok"))
 }
 
 // func deleteUserAways(w http.ResponseWriter, r *http.Request) {
@@ -153,6 +159,15 @@ func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 
+	var ListAllAwaysCmd = &cobra.Command{
+		Use:   "listall",
+		Short: "List all aways for today",
+		Long:  "List all aways for today",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return listAllAwaysCommandF(args, w, command)
+		},
+	}
+
 	var DeleteUserAwaysCmd = &cobra.Command{
 		Use:   "remove",
 		Short: "Remove your aways",
@@ -167,7 +182,7 @@ func slashCommandHandler(w http.ResponseWriter, r *http.Request) {
 	rootCmd.SetArgs(strings.Fields(strings.TrimSpace(command.Text)))
 	rootCmd.SetOutput(outBuf)
 
-	rootCmd.AddCommand(saveAwayCmd, ListUserAwaysCmd, DeleteUserAwaysCmd)
+	rootCmd.AddCommand(saveAwayCmd, ListUserAwaysCmd, ListAllAwaysCmd, DeleteUserAwaysCmd)
 
 	err = rootCmd.Execute()
 
@@ -240,6 +255,53 @@ func listUserAwaysCommandF(args []string, w http.ResponseWriter, slashCommand *m
 	payload := model.MMSlashResponse{
 		ResponseType: "ephemeral",
 		Text:         "List of Aways for user: " + slashCommand.Username,
+		Username:     "TimeAway",
+		IconUrl:      "https://png.icons8.com/ios/1600/sunbathe.png",
+		Attachments:  attachment1,
+	}
+
+	b, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Unable to marshal response")
+		return model.NewLocAppError("Mattermost Time Away", "Unable to marshal response", nil, "")
+	}
+
+	fmt.Println(payload.ToJson())
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(b)
+	return nil
+}
+
+func listAllAwaysCommandF(args []string, w http.ResponseWriter, slashCommand *model.MMSlashCommand) error {
+	t, err := time.Parse(LAYOUT, time.Now().Format(LAYOUT))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var aways []*model.Away
+	if result := <-Srv.Store.Away().GetAwaysForToday(strconv.FormatInt(t.Unix(), 10)); result.Err != nil {
+		fmt.Println(result.Err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return result.Err
+	} else {
+		aways = result.Data.([]*model.Away)
+	}
+
+	attachment1 := []model.MMAttachment{}
+	for _, away := range aways {
+		startInt, _ := strconv.ParseInt(away.Start, 10, 64)
+		endInt, _ := strconv.ParseInt(away.End, 10, 64)
+
+		msg := fmt.Sprintf("User: @%v From: %v To: %v Reason: %v", away.UserName, time.Unix(startInt, 0).Format(LAYOUT), time.Unix(endInt, 0).Format(LAYOUT), away.Reason)
+
+		attach := model.MMAttachment{}
+		// attachment1 = append(attachment1, *attach.AddField(model.MMField{Title: "Time Away", Value: msg}).AddAction(model.MMAction{Name: "Delete", Integration: &model.MMActionIntegration{URL: "http://localhost:8087/delete", Context: model.StringInterface{"id": away.Id}}}))
+		attachment1 = append(attachment1, *attach.AddField(model.MMField{Title: "Time Away", Value: msg}))
+	}
+
+	payload := model.MMSlashResponse{
+		ResponseType: "ephemeral",
+		Text:         "List of Aways for all users",
 		Username:     "TimeAway",
 		IconUrl:      "https://png.icons8.com/ios/1600/sunbathe.png",
 		Attachments:  attachment1,
